550b846635f5fb63709b974434922287a7acc83e
kimmmss <145826834+kimmmss@users.noreply.github.com>
2023-09-30 16:39:08 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# print('kjhiuhiu')
+
+# age1 =  int(input("возраст 1"))
+# age2 = int(input("возраст 2"))
+# age3= int(input("возраст 3"))
+
+# if age3>age1 and age3>age2:
+#     print("возраст 3 больше всех")
+# elif age2>age1 and age2>age3:
+#     print("возраст 2 больше всех")
+# else:
+#     print("возраст 1 больше всех")
+
+age1 = 1000
+age2 = 500
+age3 = 200
+
+if age1>age2:
+    print("денег хватит")
+elif age2>age3:
+    print("денег не хватит")
